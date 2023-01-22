0edfc61b0cc68cc4adca8d2019557a1c4ea50f56
solomonp <solodax46@gamil.com>
2023-01-22 15:35:31 +0530

    new-modifeid

--- preactapp/src/App.js
+++ preactapp/src/App.js
@@ -9,13 +9,11 @@ function App() {
   const { password, username } = data
   
   const  inputHandler = (e) => {
-   setData( e.target.name)
+   setData({...data,[e.target.name] :[  e.target.name]})
   } 
 
-  const inputHannd = (e) => {
-    setData(  e.target.value)
     
-  }
+  
 
     const  submitHandler = (e) => {
     e.preventDefault()
@@ -29,7 +27,7 @@ function App() {
       <form onSubmit={submitHandler}>
         <input type="text" value={username} name="username" onChange={inputHandler} />
         <br />
-        <input type="password" value={password} name ="password" onChange={inputHannd}/><br />
+        <input type="password" value={password} name ="password" onChange={inputHandler}/><br />
         <input type="submit" name ="sumit"/>
       </form>
     </center>
